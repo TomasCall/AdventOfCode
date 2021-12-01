ea319a631c63138b4fbd06f71df7e972a5618730
TomasCall <77113685+TomasCall@users.noreply.github.com>
2021-12-01 23:36:17 +0100

    First Day Of Christmas!!

---
+++ 1.Day/sonarA.py
@@ -0,0 +1,14 @@
+
+
+def main():
+    file = open("input.txt")
+    lines  = file.read().splitlines()
+    counter = 0
+    for i in range(1,len(lines)):
+        if int(lines[i]) > int(lines[i-1]):
+            counter += 1
+    print(counter)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 1.Day/sonarB.py
@@ -0,0 +1,21 @@
+
+def main():
+    file = open("input.txt")
+    lines  = file.read().splitlines()
+    sum_of_three = []
+    for i in range(len(lines)):
+        try:
+            sum_of_three.append(int(lines[i]) + int(lines[i+1]) + int(lines[i+2]))
+        except:
+            break
+    counter = 0
+    for i in range(1,len(sum_of_three)):
+        if sum_of_three[i] > sum_of_three[i-1]:
+            counter += 1
+    print(counter)
+    print(sum_of_three)
+
+
+
+if __name__ == "__main__":
+    main()
